014616f47b74e543c5f93346c1ac120f5b00ad94
gill104 <grrr104@hotmail.com>
2018-03-29 03:56:45 -0500

    made a json to read into rows and bs grid for pictures. WIP

--- bread.php
+++ bread.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
-<title></title>
+    <head>
+    <title>
+    </title>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -10,36 +12,73 @@
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="mainStyleSheet.css">
+
+        <script>
+        $(document).ready(function()
+        {
+            var jsonURL = "breadImages.json";
+            $.getJSON(jsonURL,function(json)
+            {
+                var imgList = "";
+                var count = 0;
+                $.each(json.empanadas, function(){
+                    if(count == 3)
+                    {   
+                        imgList+= '</div>';
+                        count = 0;
+                    }
+                    
+                    if(count > 2 || count == 0){
+                        imgList += '<div class="row">';
+                    }
+                    imgList +=  '<div class="col-sm-4 resize-img">' + 
+                                    '<img class="img-thumbnail" src="' + this.imgPath + '">' +
+                                '</div>';
+                    count++;
+                });
+               
+                $('#breadLi').append(imgList);
+                console.log(imgList);
+
+                //document.getElementById("breadLi").innerHTML=imgList;
+                });
+            });
+        </script>
+    </head>
     <body>
         <?php include("navigationBar.php"); ?>
         <p style="margin-top:70px" >This is another page!!!!</p>
 
         <div class="container">
             <div class="row">
-                <div class="col-sm-3 resize-img" >
+                <div class="col-sm-4 resize-img" >
                     <img class="img-thumbnail" src="Images/AmaConcha.png" alt="yellow shell" />
                     <p>This is a concha that we have</p>
                 </div>
-                <div class="col-sm-3 resize-img">
+                <div class="col-sm-4 resize-img">
                     <img class="img-thumbnail" src="Images/RosaConcha.png" alt="pink shell" />
                 </div>
-                <div class="col-sm-3 resize-img">
+                <div class="col-sm-4 resize-img">
+                    <img class="img-thumbnail" src="Images/ChocoConcha.jpg" alt="brown shell"/>
+                </div>
+                <div class="col-sm-4 resize-img" >
+                    <img class="img-thumbnail" src="Images/emp-pine.jpg" alt="yellow shell" />
+                    <p>This is a concha that we have</p>
+                </div>
+                <div class="col-sm-4 resize-img">
+                    <img class="img-thumbnail" src="Images/emp-pump.jpg" alt="pink shell" />
+                </div>
+                <div class="col-sm-4 resize-img">
                     <img class="img-thumbnail" src="Images/ChocoConcha.jpg" alt="brown shell"/>
                 </div>
             </div>
+            <div class="row" style="background-color:red;">
+               
+            </div>
         </div>
-        <!--
-        <ul>
-            <li class="resize-img">
-            <img src="Images/AmaConcha.png" alt="yellow shell"/>
-            </li>
-            <li class="resize-img">
-            <img src="Images/ChocoConcha.jpg" alt="brown shell"/>
-            </li>
-            <li class="resize-img">            
-                <img src="Images/RosaConcha.png" alt="pink shell" />
-            </li>
-        </ul>
-        -->
+        <div id="breadLi" class="container">
+        </div>
+
+       
     </body>
 </html>
