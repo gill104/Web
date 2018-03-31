e1c1b4291481d0e797a7197a03cce49258c8fe3b
gill104 <grrr104@hotmail.com>
2018-03-31 06:34:42 -0500

    got preloading working, maybe still need to double check....

--- bread.php
+++ bread.php
@@ -14,17 +14,26 @@
         <link rel="stylesheet" type="text/css" href="mainStyleSheet.css">
 
         <script>
+	
+		//modify this script to preload images instead.
+		//Get the paths of the images then once preloaded show them?
         $(document).ready(function()
         {
             var jsonURL = "breadImages.json";
             $.getJSON(jsonURL,function(json)
             {
+				
+			
                 var imgList = "";
                 var count = 0;
+				var classdef = 0;
                 var items = [json.empanadas, json.toast, json.sponge, json.pasteries];
                 for(var x = 0; x < items.length; x++)
                 {
                     $.each(items[x], function(){
+						var my_img = new Image();
+						my_img.src = this.imgPath;
+						console.log('first created: ' + my_img.src);
                         if(count == 4)
                         {   
                             imgList+= '</div>';
@@ -34,26 +43,63 @@
                         if(count > 3 || count == 0){
                             imgList += '<div class="row">';
                         }
-                        imgList +=  '<div class="col-sm-3 resize-img">' + 
-                                        '<img class="img-thumbnail" src="' + this.imgPath + '">' +
+                        imgList +=  '<div class="col-sm-3 resize-img ">' + 
+                                        '<img id="'+classdef+'"class="img-thumbnail imgArr" src=#>' +
                                     '</div>';
-                        
+								
                         count++;
+						classdef++;
                     });
                 }
-                    $('#breadLi').append(imgList);
+
+                    $('#breadLi').append(imgList);//puts into a container below
                     console.log(imgList);
                 
                 //document.getElementById("breadLi").innerHTML=imgList;
                 });
             });
         </script>
+	
     </head>
     <body>
         <?php include("navigationBar.php"); ?>
         <p style="margin-top:70px" >This is another page!!!!</p>
         <div id="breadLi" class="container">
         </div>
+		<div onload="preLoad();">
+		</div>
+		<script>
+			//function preLoad()
+			//{
+			$(document).ready(function()//runs at this point while file being read
+			{
+				console.log('doing stuff inthis scrupt!!!');
+					var jsonURL = "breadImages.json";
+					var counter = 0;
+					$.getJSON(jsonURL,function(json)
+					{
+						var items = [json.empanadas, json.toast, json.sponge, json.pasteries];
+						for(var x = 0; x < items.length; x++)
+						{
+							 $.each(items[x], function(){
+								//var my_img = new Image();
+								//my_img.src = this.imgPath;
+								console.log(this.imgPath);
+								console.log(counter);
+						
+								runit(counter, this.imgPath);
+								counter++;
+							});
+						}
+					});	
+			
+				function runit(idVal, img)
+				{
+					document.getElementById(idVal).src = img;
+				}
+			});
+		
+		</script>
 
        
     </body>
